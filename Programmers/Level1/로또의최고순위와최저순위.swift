f12186680e365ebd7d388e511dac6bf65f0e8bb6
Minny27 <dltmdals0608@naver.com>
2021-07-09 01:30:02 +0900

    Create 로또의최고순위와최저순위 [2021.07.09]

--- Programmers/Level1/로또의최고순위와최저순위.swift
+++ Programmers/Level1/로또의최고순위와최저순위.swift
@@ -3,57 +3,25 @@
 
 import Foundation
 
-func solution(_ rows:Int, _ columns:Int, _ queries:[[Int]]) -> [Int] {
-    var ans = [Int]()
-    var g : [[Int]] = Array(repeating: Array(repeating: 0, count: columns), count: rows)
-    var k = 1
-    for i in 0..<rows {
-        for j in 0..<columns {
-            g[i][j] = k
-            k += 1
-        }
-    }
+func solution(_ lottos:[Int], _ win_nums:[Int]) -> [Int] {
+    var winCount = 0
+    var zeroCount = 0
+    var maxValue = 0
+    var minValue = Int.max
+    let ht : [Int : Int] = [6 : 1, 5 : 2, 4 : 3, 3 : 4, 2 : 5, 1 : 6, 0 : 6]
     
-    func rotate(_ r1:Int, _ c1:Int, _ r2:Int, _ c2:Int) -> Int {
-        var tmp = g[r1 - 1][c1 - 1]
-        var minValue = tmp
-        var (i, j) = (r1 - 1, c1 - 1)
-        //오른쪽
-        for j in c1..<c2{ // 2 4
-            minValue = min(minValue, g[i][j])
-            (tmp, g[i][j]) = (g[i][j], tmp)
+    for lotto in lottos {
+        if lotto == 0 {
+            zeroCount += 1
+            continue
         }
-        j = c2 - 1
-
-        //아래
-        for i in r1..<r2{ // 2 5
-            minValue = min(minValue, g[i][j])
-            (tmp, g[i][j]) = (g[i][j], tmp)
+        for winNum in win_nums {
+            if lotto == winNum {
+                winCount += 1   
+            }
         }
-        i = r2 - 1
-        
-        //왼쪽
-        for j in stride(from: c2 - 2, to: c1 - 2, by: -1) {
-            minValue = min(minValue, g[i][j])
-            (tmp, g[i][j]) = (g[i][j], tmp)
-        }
-        j = c1 - 1
-
-        //위
-        for i in stride(from: r2 - 2, to: r1 - 2, by: -1) {
-            minValue = min(minValue, g[i][j])
-            (tmp, g[i][j]) = (g[i][j], tmp)
-        }
-        
-
-        return minValue
     }
-    for matrix in queries {
-        ans.append(rotate(matrix[0], matrix[1], matrix[2], matrix[3]))
-    }
-    return ans
+    maxValue = ht[winCount + zeroCount]!
+    minValue = ht[winCount]!
+    return [maxValue, minValue]
 }
-
-print(solution(6, 6, [[2,2,5,4],[3,3,6,6],[5,1,6,3]]))
-print(solution(3, 3, [[1,1,2,2],[1,2,2,3],[2,1,3,2],[2,2,3,3]]))
-print(solution(100, 97, [[1,1,100,97]]))
