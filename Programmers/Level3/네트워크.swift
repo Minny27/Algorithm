eabe0f15bebeda4ec2e007860d84cc7b2f2409f3
Minny27 <dltmdals0608@naver.com>
2021-09-08 19:24:19 +0900

    Update 네트워크 [2021.09.08]

--- Programmers/Level3/네트워크.swift
+++ Programmers/Level3/네트워크.swift
@@ -3,32 +3,39 @@
 
 import Foundation
 
-func solution(_ n:Int, _ computers:[[Int]]) -> Int {
-    var visited = Array(repeating: Bool(), count: n)
-    var ans = 0
+var visited = [Bool]()
 
-    func dfs(_ node: Int) -> Int {
-        if visited[node] == true {
-            return 0
-        }
-        visited[node] = true
-        for adj in 0..<n {
-            if computers[node][adj] == 1 {
-                dfs(adj)
-            }
-        }
-        
-        return 1
-    }
-    
+func solution(_ n:Int, _ computers:[[Int]]) -> Int {
+    visited = Array(repeating: Bool(), count: n)
+    var ans = 0
     for i in 0..<n {
         if !visited[i] {
-            ans += dfs(i)
+            ans += dfs(i, n, computers)
+        }
+    }
+
+    return ans
+}
+
+func dfs(_ node: Int, _ n: Int, _ computers: [[Int]]) -> Int {
+    if visited[node] == true {
+        return 0
+    }
+
+    visited[node] = true
+
+    // 한 노드에서 전체 노드 방문
+    for adj in 0..<n {
+        // 연결되어 있는 인접 노드라면 인접노드를 기준으로 dfs 시작
+        if computers[node][adj] == 1 {
+            dfs(adj, n, computers)
         }
     }
     
-    return ans
+    // 연결되어 있는 모든 노드를 방문한 것을 1로 리턴
+    return 1
 }
+
 
 // func solution(_ n:Int, _ computers:[[Int]]) -> Int {
     
