8a94b91146eb9ece579c12ddb336c3b0f6959b23
Minny27 <dltmdals0608@naver.com>
2021-09-18 10:55:41 +0900

    Create 16946_이승민_벽부수고이동하기4

---
+++ BOJ/123.DFS+BFS/16946.swift
@@ -0,0 +1,65 @@
+// 벽 부수고 이동하기 4
+
+import Foundation
+
+let input = readLine()!.components(separatedBy: " ").map { Int(String($0))! }
+let (n, m) = (input[0], input[1])
+var g = Array(repeating: Array(repeating: 0, count: m), count: n)
+for i in 0..<n {
+    let line = Array(readLine()!).map { Int(String($0))! }
+    for j in 0..<m {
+        g[i][j] = line[j]
+    }
+}
+var ans = g
+
+
+
+let d = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+func boundary(_ i: Int, _ j: Int) -> Bool {
+    return 0 <= i && i < n && 0 <= j && j < m
+}
+
+func bfs(_ i: Int, _ j: Int) -> Int {
+    var q = [[Int]]()
+    var visited = Set<[Int]>()
+    var cnt = 1
+    q.append([i, j])
+    visited.insert([i, j])
+    
+    while !q.isEmpty {
+        let (i, j) = (q[0][0], q[0][1])
+        q.removeFirst()
+        for k in d {
+            let (ni, nj) = (i + k[0], j + k[1])
+            
+            if !boundary(ni, nj) || visited.contains([ni, nj]) {
+                continue
+            }
+            
+            if g[ni][nj] == 0 {
+                cnt += 1
+                visited.insert([ni, nj])
+                q.append([ni, nj])
+            }
+        }
+    }
+    
+    return cnt
+}
+
+for i in 0..<n {
+    for j in 0..<m {
+        if g[i][j] == 1 {
+            ans[i][j] = bfs(i, j)
+        }
+    }
+}
+
+for i in 0..<n {
+    for j in 0..<m {
+        print(ans[i][j], terminator: "")
+    }
+    print()
+}
